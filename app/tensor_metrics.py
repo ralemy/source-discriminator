88c4358b9a9958f0c846732846a18b1974c2733f
Reza Alemy <reza@alemy.net>
2021-10-25 07:11:22 -0700

    removed tf.function

--- app/tensor_metrics.py
+++ app/tensor_metrics.py
@@ -25,27 +25,22 @@ class TensorMetrics:
             'global': tf.summary.create_file_writer(os.path.join(log_base, 'global'))
         }
 
-    @tf.function
     def reset_epoch_metrics(self):
         for x in ['train', 'test']:
             for y in ['loss', 'accuracy']:
                 self.metrics[y][x].reset_states()
 
-    @tf.function
     def write_metric_set(self, epoch, role='train'):
         with self.summary_writer[role].as_default():
             tf.summary.scalar('loss', self.metrics['loss'][role].result(), step=epoch)
             tf.summary.scalar('accuracy', self.metrics['accuracy'][role].result(), step=epoch)
 
-    @tf.function
     def update_loss(self, role, loss):
         self.metrics['loss'][role](loss)
 
-    @tf.function
     def update_accuracy(self, role, expected, actual):
         self.metrics['accuracy'][role](expected, actual)
 
-    @tf.function
     def report_epoch(self,epoch):
         self.write_metric_set(epoch, 'train')
         self.write_metric_set(epoch, 'test')
