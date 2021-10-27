bc5339fe2e4a42ff4a5500f3fddd76c5fb2df31c
Reza Alemy <reza@alemy.net>
2021-10-26 23:33:05 -0700

    11-7x5

--- app/contrib.py
+++ app/contrib.py
@@ -177,7 +177,7 @@ class ResNetTypeX(tf.keras.Model): #Custom added type
 
         self.layer1 = make_basic_block_layer(filter_num=8,
                                              blocks=layer_params[0], 
-                                             stride=2, kernel_size= (3,3))
+                                             stride=2, kernel_size= (7,7))
         self.layer2 = make_basic_block_layer(filter_num=16,
                                              blocks=layer_params[1],
                                              stride=2, kernel_size=(3,3))
